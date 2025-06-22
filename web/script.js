ea05ff06f67a7773a67868e319a70ec67866a912
Shelby Jueden <shelby@jueden.org>
2025-06-21 17:48:10 -0700

    sanity check

--- web/script.js
+++ web/script.js
@@ -979,11 +979,6 @@ function onTouchUp(event) {
     if (touches.length != 1) return;
     for (const touch of touches) {
         onMouseUp(null,touch.pageX,  touch.pageY);
-        if (touch_count < 2 && !touch_start_dual)
-        {
-            dragStartPosition = getTransformedPoint(touch.pageX,  touch.pageY);
-            touch_start_dual=true;
-        }
     }
 
 }
