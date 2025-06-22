47e62956a349452b5a9d8fc76366b00991d1ba33
Shelby Jueden <shelby@jueden.org>
2025-06-21 20:03:05 -0700

    Add data

---
+++ web/data.js
@@ -0,0 +1,268 @@
+track_names=[
+    {
+        "name":"Acorn Heights",
+        "map_position":[927,83],
+        "map_offset": [29, 29]
+    },
+{
+    "name":"Airship Fortress",
+    "map_position":[490,299],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Boo Cinema",
+    "map_position":[1099,152],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Bowser's Castle",
+    "map_position":[633,195],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Cheep Cheep Falls",
+    "map_position":[1110,369],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Choco Mountain",
+    "map_position":[775,539],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Crown City",
+    "map_position":[765,780],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Dandelion Depths",
+    "map_position":[1094,558],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Desert Hills",
+    "map_position":[446,751],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Dino Dino Jungle",
+    "map_position":[1116,934],
+    "map_offset": [29, 29]
+},
+{
+    "name":"DK Pass",
+    "map_position":[1220,424],
+    "map_offset": [29, 29]
+},
+{
+    "name":"DK Spaceport",
+    "map_position":[738,941],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Dry Bones Burnout",
+    "map_position":[797,165],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Faraway Oasis",
+    "map_position":[1096,757],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Great ? Block Ruins",
+    "map_position":[1285,878],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Koopa Troopa Beach",
+    "map_position":[927,866],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Mario Bros. Circuit",
+    "map_position":[588,656],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Mario Circuit",
+    "map_position":[939,272],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Moo Moo Meadows",
+    "map_position":[933,451],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Peach Beach",
+    "map_position":[1375,737],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Peach Stadium",
+    "map_position":[932,620],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Salty Salty Speedway",
+    "map_position":[1256,663],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Shy Guy Bazaar",
+    "map_position":[451,552],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Sky-High Sundae",
+    "map_position":[1377,352],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Starview Peak",
+    "map_position":[1237,250],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Toad's Factory",
+    "map_position":[770,350],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Wario Shipyard",
+    "map_position":[1410,550],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Wario Stadium",
+    "map_position":[637,443],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Whistlestop Summit",
+    "map_position":[602,848],
+    "map_offset": [29, 29]
+}
+];
+
+
+character_locations=[
+{
+    "name":"Pauline",
+    "map_position":[713,832],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Mario",
+    "map_position":[609,772],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Daisy",
+    "map_position":[483,636],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Toadette",
+    "map_position":[726,645],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Luigi",
+    "map_position":[602,526],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Shy Guy",
+    "map_position":[488,469],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Waluigi",
+    "map_position":[685,386],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Bowser Jr.",
+    "map_position":[602,332],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Bowser",
+    "map_position":[719,262],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Baby Luigi",
+    "map_position":[806,479],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Toad",
+    "map_position":[869,356],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Peach",
+    "map_position":[935,533],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Baby Daisy",
+    "map_position":[962,170],
+    "map_offset": [29, 29]
+},
+{
+    "name":"King Boo",
+    "map_position":[1050,248],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Baby Mario",
+    "map_position":[1053,449],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Rosalina",
+    "map_position":[1230,307],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Donkey Kong",
+    "map_position":[1163,482],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Birdo",
+    "map_position":[1206,601],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Yoshi",
+    "map_position":[1087,665],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Baby Peach",
+    "map_position":[1252,769],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Lakitu",
+    "map_position":[1192,875],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Koopa Troopa",
+    "map_position":[1018,871],
+    "map_offset": [29, 29]
+},
+{
+    "name":"Baby Rosalina",
+    "map_position":[912,874],
+    "map_offset": [29, 29]
+}
+];
+
+character_locations.sort((a, b) => a.name.localeCompare(b.name))
