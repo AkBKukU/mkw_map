bb14267fcb484cc10d0c40a4bd6f8dbae9a416ce
Shelby Jueden <shelby@jueden.org>
2025-06-21 18:35:48 -0700

    min width to not collide menus

--- web/script.js
+++ web/script.js
@@ -1650,7 +1650,7 @@ function map_initialize()
 map_initialize();
 
 function isMobile() {
-  const minWidth = 768; // Minimum width for desktop devices
+  const minWidth = 900; // Minimum width for desktop devices
   return window.innerWidth < minWidth || screen.width < minWidth;
 }
 
